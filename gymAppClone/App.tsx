427469e653c5f3f28ba7bba443590b7a42f4425d
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-11-13 18:16:34 +0300

    Update App.tsx

--- gymAppClone/App.tsx
+++ gymAppClone/App.tsx
@@ -1,45 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function MainScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>main!</Text>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Details!</Text>
+    </View>
+  );
+}
+function StudioScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Studio!</Text>
+    </View>
+  );
+}
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'lightblue'}}>
-      <Text style={{textAlign: 'center'}}>Home</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home screen</Text>
       <Button
-        title="Go to details"
-        onPress={() => navigation.navigate('Musab')}
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
       />
     </View>
   );
 }
-function MusabScreen() {
+
+function SettingsScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'red'}}>
-      <Text>Details</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings screen</Text>
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
+const AnotherStack = createNativeStackNavigator();
 
-function App() {
+function AnotherStackScreen() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Musab" component={MusabScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AnotherStack.Navigator>
+      <AnotherStack.Screen name="Home" component={HomeScreen} />
+      <AnotherStack.Screen name="Details" component={DetailsScreen} />
+    </AnotherStack.Navigator>
   );
 }
 
-export default App;
+function DataScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Ölçümler screen</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false}}>
+        <Tab.Screen name="Ana sayfa" component={MainScreen} />
+        <Tab.Screen name="Ölçümler" component={DataScreen} />
+        <Tab.Screen name="Stüdyo" component={DetailsScreen} />
+        <Tab.Screen name="Diğer" component={AnotherStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
