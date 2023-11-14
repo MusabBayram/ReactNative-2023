0e1956207ef7d13952f6a199d9660b479a91404e
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2023-11-14 13:34:12 +0300

    Update App.tsx

--- gymAppClone/App.tsx
+++ gymAppClone/App.tsx
@@ -3,68 +3,11 @@ import { Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/pages/HomeScreen';
+import DataScreen from './src/pages/DataScreen';
+import StudioScreen from './src/pages/StudioScreen';
+import AnotherStackScreen from './src/pages/AnotherStackScreen';
 
-function MainScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>main!</Text>
-    </View>
-  );
-}
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Details!</Text>
-    </View>
-  );
-}
-function StudioScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Studio!</Text>
-    </View>
-  );
-}
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-function SettingsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings screen</Text>
-    </View>
-  );
-}
-
-const AnotherStack = createNativeStackNavigator();
-
-function AnotherStackScreen() {
-  return (
-    <AnotherStack.Navigator>
-      <AnotherStack.Screen name="Home" component={HomeScreen} />
-      <AnotherStack.Screen name="Details" component={DetailsScreen} />
-    </AnotherStack.Navigator>
-  );
-}
-
-function DataScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Ölçümler screen</Text>
-    </View>
-  );
-}
 
 const Tab = createBottomTabNavigator();
 
@@ -72,9 +15,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator screenOptions={{ headerShown: false}}>
-        <Tab.Screen name="Ana sayfa" component={MainScreen} />
+        <Tab.Screen name="Ana sayfa" component={HomeScreen} />
         <Tab.Screen name="Ölçümler" component={DataScreen} />
-        <Tab.Screen name="Stüdyo" component={DetailsScreen} />
+        <Tab.Screen name="Stüdyo" component={StudioScreen} />
         <Tab.Screen name="Diğer" component={AnotherStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
